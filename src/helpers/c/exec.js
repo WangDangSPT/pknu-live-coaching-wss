f971b0768c0d66e92a5b12cdc9aef3cdf247f183
wangdang <elkanaalifi@gmail.com>
2022-05-12 17:44:40 +0900

    added compile module, test folder

--- src/helpers/c/exec.js
+++ src/helpers/c/exec.js
@@ -1,23 +1,46 @@
 const {spawn} = require('child_process')
-const {writeFile} = require('fs');
+const {writeFileSync} = require('fs');
 
 //need to wrap this in a promise
-let compile = (sourceCode)=>{
+let compileString = (sourceCode)=>{
     //convert string to .c file
     let output = ''
-    writeFile('source.c',sourceCode,err =>{
+    writeFileSync('test/source.c',sourceCode,err =>{
         if(err){
             console.log(`error: ${err}`);
         }
     })
-    //child process to exec output.o iff source.c is compiled
-    let child = spawn('gcc -o output.o source.c && ./output.o', {
-        stdio: 'inherit',
-        shell: true
-    })
-    child.stdout.on('data', (data) =>{
-        return output+=data;
-    })
+    return compileFile()
     
 }
-module.exports.compile
+
+let compileFile = () =>{
+    let output = ''
+    let child = spawn(`gcc -o test/output.out test/source.c`,{
+        shell: true
+    })
+    child.stderr.on('data', (data=>{
+        console.error(`stderr : ${data}`)
+    }))
+    child.on('exit', (code,signal)=>{
+        //singal == null when process exits normally
+        if(signal === null){
+            let child2 = spawn('test/output.out', {
+                // dont use stdio:inherit
+                shell: true
+                })
+                child2.stdout.on('data', (data) =>{
+                    // return output+=data;
+                    output += data.toString()
+                    return output;
+                })
+                child2.stderr.on('data', (data=>{
+                    console.error(`stderr : ${data}`)
+                    // return output = `error while executing source file, stderr : ${data}`
+                }))
+        }
+    })
+
+    
+}
+module.exports.compileFile = compileFile
---
+++ test/test.js
@@ -0,0 +1,6 @@
+const compile = require('../src/helpers/c/exec')
+let output = compile.compileFile()
+
+let compileOutput = new Promise((resolve,reject)=>{
+    
+})
